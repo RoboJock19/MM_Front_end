6682ad1961ad975f0908e41479b90267b9da6590
RoboJock19 <steven.lance.w@gmail.com>
2022-08-30 09:51:32 +0200

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,10 @@
+var navbar = ` 
+<div class="topnav" id="myTopnav">
+  <a href="bots.html">Bots</a>
+  <a href="BotSwitch.html">Switchboard</a>
+  <a href="server_check.html">Server</a>
+
+</div>`;
+
+// inserting navbar in beginning of body
+document.body.insertAdjacentHTML("afterbegin", navbar);
